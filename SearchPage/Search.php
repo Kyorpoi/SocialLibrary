a283f3cca5651c90a221f50b8ff4274a060d920a
凛冬 <384693801@qq.com>
2019-04-27 08:54:31 +0800

    返回结果

--- SearchPage/Search.php
+++ SearchPage/Search.php
@@ -1,15 +1,23 @@
 <?php
     require 'Medoo.php';
-    //require 'config.php';
-   // print_r($database->info());  
-    //echo "<h1>php通用数据库db类Medoo使用示例</h1>";
-    $database = new medoo('my_database');
-    $datas = $database->select("94im", [
+    use Medoo\Medoo;
+    $database = new Medoo([
+        'database_type' => 'pgsql',
+        'database_name' => 'Data',
+        'server' => '192.168.23.129',
+        'username' => 'postgres',
+        'password' => '851e28c69d050650',
+    ]);
+
+    $result = $database -> select("106w", [
         "Username",
         "Password"
     ], [
-        "Password"[~]" => 123%
+        "Password[~]" => 123
     ]);
-    print_r($database->info());  
-    var_dump($database->error());
+    print_r($result);
+    var_dump( $database->log() );
+    //print_r($database->info());  
+    echo "<br>";
+    echo array_keys($result);
 ?>
--- SearchPage/config.php
+++ SearchPage/config.php
@@ -1,13 +1,9 @@
-<?php
-   $host        = "host=192.168.23.129";
-   $port        = "port=5432";
-   $dbname      = "dbname=Data  ";
-   $credentials = "user=postgres password=851e28c69d050650";
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db){
-      echo "Error : Unable to open database";
-   } 
-   else {
-      echo "Opened database successfully<br>";
-   }
+<?
+    $database = new Medoo([
+        'database_type' => 'pgsql',
+        'database_name' => 'Data',
+        'server' => '192.168.23.129',
+        'username' => 'postgres',
+        'password' => '851e28c69d050650',
+    ]);
 ?>
