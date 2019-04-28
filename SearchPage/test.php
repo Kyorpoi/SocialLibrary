a1d39da3b205212fbc56e95d89d596b148f44db5
凛冬 <384693801@qq.com>
2019-04-28 19:54:10 +0800

    ajax测试

--- SearchPage/test.php
+++ SearchPage/test.php
@@ -1,1 +1,18 @@
-<?php phpinfo();?>
+<?php
+header('content-type:application:json;charset=utf8');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+$a = $_GET['a'];
+$b = $_GET['b'];
+$cars=array("Volvo"=>"9","BMW"=>"9","Toyota"=>"5");
+$c = $a + $b;
+
+echo($c);
+echo "<br>";
+print_r(array_keys($cars));
+echo "<br>";
+print_r($c);
+echo "<br>";
+var_dump($c);
+?>
